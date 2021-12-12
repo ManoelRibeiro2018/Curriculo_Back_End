bba0aae53baeb86642e955b53a8768c0712ce70d
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-12-12 16:23:11 -0300

    Criando Actions

--- Controllers/CurriculoController.cs
+++ Controllers/CurriculoController.cs
@@ -6,10 +6,37 @@ namespace Curriculo.Controllers
     [Route("api/curriculo")]
     public class CurriculoController : ControllerBase
     {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
         [HttpPost]
         public IActionResult Post([FromBody] CurriculoMain curriculo)
         {
             return Ok();
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        
     }
 }
--- Model/CurriculoMain.cs
+++ Model/CurriculoMain.cs
@@ -8,5 +8,8 @@ namespace Curriculo.Model
     public class CurriculoMain : EntityBase
     {
         public string Title { get; set; }
+        public User User { get; set; }
+        public List<Experience> Experiences { get; set; }
+        public List<Hobby>  Hobbies { get; set; }
     }
 }
